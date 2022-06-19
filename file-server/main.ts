20430916c2a298f5e64386a9e5e99047496ebefe
topheruk <kristopherab@gmail.com>
2022-06-19 20:35:23 +0000

    file server example

--- file-server/main.ts
+++ file-server/main.ts
@@ -1,1 +1,13 @@
-console.log("hello, world");
+import * as http from "../../util/mod.ts";
+
+const mux = new http.ServeMux();
+
+const fs = http.fileServer("../views");
+
+const sp = http.stripPrefix("/views", fs);
+
+mux.handle("/views/*", sp);
+
+const srv = new http.Server({ port: 7878, handler: mux });
+
+await srv.listenAndServe();
